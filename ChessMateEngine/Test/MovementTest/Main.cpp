e0119f1e6871bae322e2dfc4e5046eed64392255
Tim Flynn <trflynn89@gmail.com>
2015-10-02 14:30:05 -0400

    Add SocketTest / MovementTest to visual studio / gtest

--- ChessMateEngine/Test/MovementTest/Main.cpp
+++ ChessMateEngine/Test/MovementTest/Main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <vector>
 
-#include <Movement/WholeMoveSet.h>
+#include <gtest/gtest.h>
+
+#include <Movement/MoveSet.h>
 #include <Util/Logging/Logger.h>
 
-int main()
+//=============================================================================
+TEST(MovementTest, FooTest)
 {
-    Movement::WholeMoveSet ms;
-    Movement::MoveLists w = ms.GetWhitePawnMoves();
+    Movement::MoveSet ms;
 
     for (int i = 0; i < Game::BOARD_SIZE; ++i)
     {
-        std::cout << w[i].size() << std::endl;
+        Movement::MoveList w = ms.GetWhitePawnMoves(i);
+        LOGC("%d", w.size());
     }
-
-    return 0;
 }
--- ChessMateEngine/Test/SocketTest/Main.cpp
+++ ChessMateEngine/Test/SocketTest/Main.cpp
@@ -3,227 +3,181 @@
 #include <memory>
 #include <string>
 
+#include <gtest/gtest.h>
+
 #include <Util/Logging/Logger.h>
 #include <Util/Socket/AsyncStructs.h>
 #include <Util/Socket/SocketManager.h>
 #include <Util/Socket/SocketManagerImpl.h>
 #include <Util/String/String.h>
 
-//=============================================================================
-Util::LoggerPtr InitLogger()
+namespace
 {
-    Util::LoggerPtr spLogger = std::make_shared<Util::Logger>();
-    Util::Logger::SetInstance(spLogger);
-
-    return spLogger;
-}
-
-//=============================================================================
-bool CreateAcceptSocket(Util::SocketManagerPtr spSocketManager, int acceptPort)
-{
-    Util::SocketPtr spSocket = spSocketManager->CreateAsyncTcpSocket();
-
-    if (spSocket)
+    //=========================================================================
+    Util::LoggerPtr InitLogger()
     {
-        if (!spSocket->BindForReuse(Util::Socket::InAddrAny(), acceptPort))
-        {
-            spSocket.reset();
-        }
+        Util::LoggerPtr spLogger = std::make_shared<Util::Logger>();
+        Util::Logger::SetInstance(spLogger);
 
-        else if (!spSocket->Listen())
-        {
-            spSocket.reset();
-        }
+        return spLogger;
     }
 
-    return (spSocket && spSocket->IsListening());
-}
-
-//=============================================================================
-Util::SocketManagerPtr InitSocketManager(int acceptPort)
-{
-    Util::SocketManagerPtr spSocketManager = std::make_shared<Util::SocketManagerImpl>();
-    spSocketManager->StartSocketManager();
-
-    if (!CreateAcceptSocket(spSocketManager, acceptPort))
+    //=========================================================================
+    bool CreateAcceptSocket(Util::SocketManagerPtr spSocketManager, int acceptPort)
     {
-        spSocketManager.reset();
-    }
+        Util::SocketPtr spSocket = spSocketManager->CreateAsyncTcpSocket();
 
-    return spSocketManager;
-}
-
-//=============================================================================
-void StopSocketManager(Util::SocketManagerPtr spSocketManager)
-{
-    if (spSocketManager)
-    {
-        spSocketManager->StopSocketManager();
-    }
-}
-
-//=============================================================================
-Util::SocketPtr ConnectNewSocket(Util::SocketManagerPtr spSocketManager, std::string host, int port)
-{
-    Util::SocketPtr spSocket = spSocketManager->CreateAsyncTcpSocket();
-
-    if (spSocket)
-    {
-        Util::Socket::ConnectedState state = spSocket->ConnectAsync(host, port);
-
-        if (state == Util::Socket::CONNECTED)
+        if (spSocket)
         {
-            LOGC("Connected immediately to %s:%d", host, port);
-        }
-        else if (state == Util::Socket::CONNECTING)
-        {
-            Util::AsyncConnect connect;
-            std::chrono::seconds waitTime(1);
-
-            if (spSocketManager->WaitForCompletedConnect(connect, waitTime))
+            if (!spSocket->BindForReuse(Util::Socket::InAddrAny(), acceptPort))
             {
-                if (spSocket->IsConnected())
+                spSocket.reset();
+            }
+
+            else if (!spSocket->Listen())
+            {
+                spSocket.reset();
+            }
+        }
+
+        return (spSocket && spSocket->IsListening());
+    }
+
+    //=========================================================================
+    Util::SocketManagerPtr InitSocketManager(int acceptPort)
+    {
+        Util::SocketManagerPtr spSocketManager = std::make_shared<Util::SocketManagerImpl>();
+        spSocketManager->StartSocketManager();
+
+        if (!CreateAcceptSocket(spSocketManager, acceptPort))
+        {
+            spSocketManager.reset();
+        }
+
+        return spSocketManager;
+    }
+
+    //=========================================================================
+    void StopSocketManager(Util::SocketManagerPtr spSocketManager)
+    {
+        if (spSocketManager)
+        {
+            spSocketManager->StopSocketManager();
+        }
+    }
+
+    //=========================================================================
+    Util::SocketPtr ConnectNewSocket(Util::SocketManagerPtr spSocketManager, std::string host, int port)
+    {
+        Util::SocketPtr spSocket = spSocketManager->CreateAsyncTcpSocket();
+
+        if (spSocket)
+        {
+            Util::Socket::ConnectedState state = spSocket->ConnectAsync(host, port);
+
+            if (state == Util::Socket::CONNECTED)
+            {
+                LOGC("Connected immediately to %s:%d", host, port);
+            }
+            else if (state == Util::Socket::CONNECTING)
+            {
+                Util::AsyncConnect connect;
+                std::chrono::seconds waitTime(1);
+
+                if (spSocketManager->WaitForCompletedConnect(connect, waitTime))
                 {
-                    LOGC("Connected asynchronously to %s:%d", host, port);
+                    if (spSocket->IsConnected())
+                    {
+                        LOGC("Connected asynchronously to %s:%d", host, port);
+                    }
+                    else
+                    {
+                        LOGC("Async connect to %s:%d was valid, but not connected", host, port);
+                        spSocket.reset();
+                    }
                 }
                 else
                 {
-                    LOGC("Async connect to %s:%d was valid, but not connected", host, port);
+                    LOGC("Not connected to %s:%d", host, port);
                     spSocket.reset();
                 }
             }
             else
             {
-                LOGC("Not connected to %s:%d", host, port);
+                LOGC("Connect to %s:%d failed", host, port);
                 spSocket.reset();
             }
         }
         else
         {
-            LOGC("Connect to %s:%d failed", host, port);
-            spSocket.reset();
+            LOGC("Could not create socket");
         }
-    }
-    else
-    {
-        LOGC("Could not create socket");
-    }
 
-    return spSocket;
+        return spSocket;
+    }
 }
 
 //=============================================================================
-bool RunSendRecvTest(Util::SocketManagerPtr spSocketManager, Util::SocketPtr spSocket, unsigned int messageLength)
+class SocketTest : public ::testing::Test
 {
-    if (!spSocket->IsValid())
+protected:
+    SocketTest() :
+        m_spLogger(InitLogger()),
+        m_spSocketManager(InitSocketManager(12390)),
+        m_spSocket(ConnectNewSocket(m_spSocketManager, "localhost", 12390))
     {
-        LOGC("Socket has been closed");
-        return false;
     }
 
-    LOGC("Generating message of length %u", messageLength);
-    const std::string message = Util::String::GenerateRandomString(messageLength);
-
-    LOGC("Client sending message of length %u", message.length());
-    auto startTime = std::chrono::high_resolution_clock::now();
-
-    if (spSocket->SendAsync(message))
+    virtual ~SocketTest()
     {
+        StopSocketManager(m_spSocketManager);
+        m_spLogger->Flush();
+    }
+
+    //=========================================================================
+    void RunSendRecvTest(unsigned int messageLength)
+    {
+        ASSERT_TRUE(m_spSocket && m_spSocket->IsValid());
+
+        LOGC("Generating message of length %u", messageLength);
+        const std::string message = Util::String::GenerateRandomString(messageLength);
+
+        LOGC("Client sending message of length %u", message.length());
+        auto startTime = std::chrono::high_resolution_clock::now();
+
+        ASSERT_TRUE(m_spSocket->SendAsync(message));
+
         Util::AsyncRequest request;
         std::chrono::seconds waitTime(120);
+        ASSERT_TRUE(m_spSocketManager->WaitForCompletedReceive(request, waitTime));
+         
+        auto endTime = std::chrono::high_resolution_clock::now();
 
-        if (spSocketManager->WaitForCompletedReceive(request, waitTime))
-        {
-            auto endTime = std::chrono::high_resolution_clock::now();
+        std::string received = request.GetRequest();
+        std::string::size_type bytesReceived = received.length();
 
-            std::string received = request.GetRequest();
-            std::string::size_type bytesReceived = received.length();
+        auto recvTime = std::chrono::duration_cast<std::chrono::duration<double>>(endTime - startTime);
+        double throughput = static_cast<double>(bytesReceived) / 1024.0 / recvTime.count();
 
-            auto recvTime = std::chrono::duration_cast<std::chrono::duration<double>>(endTime - startTime);
-            double throughput = static_cast<double>(bytesReceived) / 1024.0 / recvTime.count();
+        LOGC("Server received message of length %u, average throughput of %f KBps",
+            bytesReceived, throughput);
 
-            LOGC("Server received message of length %u, average throughput of %f KBps",
-                bytesReceived, throughput);
-
-            return (message == received);
-        }
-        else
-        {
-            LOGC("Server did not receive message");
-        }
-    }
-    else
-    {
-        LOGC("Error sending message");
+        ASSERT_EQ(message, received);
     }
 
-    return false;
+    Util::LoggerPtr m_spLogger;
+    Util::SocketManagerPtr m_spSocketManager;
+    Util::SocketPtr m_spSocket;
+};
+
+//=============================================================================
+TEST_F(SocketTest, SmallSendRecvTest)
+{
+    RunSendRecvTest(128);
 }
 
 //=============================================================================
-int main()
+TEST_F(SocketTest, LargeSendRecvTest)
 {
-    int exitCode = 0;
-
-    Util::LoggerPtr spLogger = InitLogger();
-
-    Util::SocketManagerPtr spSocketManager = InitSocketManager(12390);
-    Util::SocketPtr spSocket;
-
-    if (spSocketManager)
-    {
-        spSocket = ConnectNewSocket(spSocketManager, "localhost", 12390);
-    }
-    else
-    {
-        LOGC("Could not create socket manager");
-        exitCode = 1;
-    }
-
-    if (spSocket && spSocket->IsValid())
-    {
-        const unsigned int smallMessageLength = 128;
-        const unsigned int largeMessageLength = 128 << 20;
-
-        if (RunSendRecvTest(spSocketManager, spSocket, smallMessageLength))
-        {
-            LOGC("Small send/recv test passed");
-        }
-        else if (exitCode == 0)
-        {
-            LOGC("Small send/recv test failed");
-            spSocket->Close();
-            exitCode = 3;
-        }
-
-        if (RunSendRecvTest(spSocketManager, spSocket, largeMessageLength))
-        {
-            LOGC("Large send/recv test passed");
-        }
-        else if (exitCode == 0)
-        {
-            LOGC("Large send/recv test failed");
-            spSocket->Close();
-            exitCode = 4;
-        }
-    }
-    else if (exitCode == 0)
-    {
-        LOGC("Could not connect new socket");
-        exitCode = 2;
-    }
-
-    StopSocketManager(spSocketManager);
-
-    // Make sure there are no lingering references to the socket manager
-    long useCount = spSocketManager.use_count();
-
-    if (useCount != 1)
-    {
-        LOGC("Too many references to socket manager: %ld", useCount);
-        exitCode = 5;
-    }
-
-    spLogger->Flush();
-    return exitCode;
+    RunSendRecvTest(128 << 10);
 }
