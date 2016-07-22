e416fac1eae486a477e301b9ed1f89f804302988
Tim Flynn <trflynn89@gmail.com>
2016-07-22 09:33:14 -0400

    Fix crash in GameManager
    Add lock around m_runningFutures

--- ChessMateEngine/Game/GameManager.cpp
+++ ChessMateEngine/Game/GameManager.cpp
@@ -90,6 +90,10 @@ void GameManager::StopRunner()
         spSocketManager->ClearClientCallbacks();
     }
 
+    StopAllGames();
+
+    std::lock_guard<std::mutex> lock(m_runningFuturesMutex);
+
     for (auto &future : m_runningFutures)
     {
         if (future.valid())
@@ -97,8 +101,6 @@ void GameManager::StopRunner()
             future.get();
         }
     }
-
-    StopAllGames();
 }
 
 //==============================================================================
@@ -211,6 +213,8 @@ void GameManager::giveRequestToGame(const Util::AsyncRequest &request)
         auto function = &GameManager::handleMessage;
 
         auto future = std::async(std::launch::async, function, spThis, spGame, message);
+
+        std::lock_guard<std::mutex> lock(m_runningFuturesMutex);
         m_runningFutures.push_back(std::move(future));
     }
 }
@@ -282,17 +286,24 @@ void GameManager::handleMessage(const ChessGamePtr spGame, const Message message
 //==============================================================================
 void GameManager::deleteFinishedFutures()
 {
-    for (auto it = m_runningFutures.begin(); it != m_runningFutures.end(); )
-    {
-        std::future_status status = it->wait_for(std::chrono::seconds::zero());
+    static const std::chrono::seconds noWait = std::chrono::seconds::zero();
 
-        if (status == std::future_status::ready)
+    std::unique_lock<std::mutex> lock(m_runningFuturesMutex, std::try_to_lock);
+
+    if (lock.owns_lock())
+    {
+        for (auto it = m_runningFutures.begin(); it != m_runningFutures.end(); )
         {
-            it = m_runningFutures.erase(it);
-        }
-        else
-        {
-            ++it;
+            std::future_status status = it->wait_for(noWait);
+
+            if (status == std::future_status::ready)
+            {
+                it = m_runningFutures.erase(it);
+            }
+            else
+            {
+                ++it;
+            }
         }
     }
 }
