6dbd05702acff5eb4ccf48c01ba2e75899421b37
Timothy Flynn <trflynn89@pm.me>
2020-08-22 15:30:38 -0400

    Fix gitignore to re-allow third-party JAR files

--- ChessMateEngine/files.mk
+++ ChessMateEngine/files.mk
@@ -1,16 +1,13 @@
-# Define the directories to include and compile
 SRC_DIRS_$(d) := \
     ChessMateEngine/engine \
     ChessMateEngine/game \
     ChessMateEngine/movement
 
-# Define compiler flags
+SRC_$(d) := \
+    $(d)/chessmate.cpp
+
 CFLAGS_$(d) += -I$(d)
 CXXFLAGS_$(d) += -I$(d)
 
-# Define libraries to link
-LDLIBS_$(d) := -lfly -lpthread
-
-# Define source files
-SRC_$(d) := \
-    $(d)/chessmate.cpp
+LDLIBS_$(d) := \
+    -lfly
--- ChessMateGUI/src/main/java/files.mk
+++ ChessMateGUI/src/main/java/files.mk
@@ -1,4 +1,3 @@
-# Define the directories to include and compile
 SRC_DIRS_$(d) := \
     ChessMateGUI/src/main/java/com/flynn/chessmate \
     ChessMateGUI/src/main/java/com/flynn/chessmate/communication \
@@ -7,13 +6,10 @@ SRC_DIRS_$(d) := \
     ChessMateGUI/src/main/java/com/flynn/chessmate/movement \
     ChessMateGUI/src/main/java/com/flynn/chessmate/util
 
-# Define the application entry point
 MAIN_CLASS_$(d) := com.flynn.chessmate.ChessMateGUI
 
-# Define JARs and paths to include on the class path
 CLASS_PATH_$(d) := \
     $(d)/../../../lib/org/freixas/tablelayout/TableLayout/1.0/TableLayout-1.0.jar
 
-# Define resources to include in the JAR
 RESOURCES_$(d) := \
     $(d)/../resources/images
